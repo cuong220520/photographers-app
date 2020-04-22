4c578d2adc176e5af01f8a522c3489f47c6e5652
cuong220520 <cuongNDGBS18195@fpt.edu.vn>
2020-04-22 13:51:47 +0700

    do user model

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const connectDB = require('./config/db')
 // connect DB
 connectDB()
 
-const PORT = process.env.PORT || 5000
-
 app.get('/', (req, res) => {
     res.send('API running')
 })
+
+// define routes
+app.use('/api/users', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/post'))
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
