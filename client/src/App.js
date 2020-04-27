cb63dee5e0238972ad879d5c3a8e4b0c6d83ae0c
cuong220520 <cuongNDGBS18195@fpt.edu.vn>
2020-04-27 14:53:53 +0700

    create logout and PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Landing from './components/layouts/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alert from './components/layouts/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
+
 import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 
@@ -40,7 +43,15 @@ const App = () => {
                                 path='/register'
                                 component={Register}
                             />
-                            <Route exact path='/login' component={Login} />
+                            <Route 
+                                exact 
+                                path='/login' 
+                                component={Login} />
+                            <PrivateRoute
+                                exact
+                                path='/dashboard'
+                                component={Dashboard}
+                            />
                         </Switch>
                     </section>
                 </Fragment>
